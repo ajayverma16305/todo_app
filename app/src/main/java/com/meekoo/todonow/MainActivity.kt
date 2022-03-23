7b072f6272b31bce9a7baf33046f5a423ab95969
Ajay Verma <nj.ajayverma@gmail.com>
2022-03-23 16:49:02 +0530

    Check commit

--- app/src/main/java/com/meekoo/todonow/MainActivity.kt
+++ app/src/main/java/com/meekoo/todonow/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.meekoo.todonow
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // will add dependency
     }
 }
